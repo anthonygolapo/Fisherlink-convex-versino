0c863193bd7c76d7ff7a3160713b25dc6c68c316
pnx <pgmarfa@envicomm.org>
2026-07-02 13:47:15 +0800

    Point frontend to production Convex deployment

--- src/js/frontend-config.js
+++ src/js/frontend-config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  CONVEX_URL: "https://giant-poodle-212.convex.cloud",
+  CONVEX_URL: "https://aromatic-pika-793.convex.cloud",
   AUTH: {
     SESSION_KEY: "fisherlink_role",
     ADMIN_PASSWORD: "admin",
