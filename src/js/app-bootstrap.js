7d9b6cf7015070dd91615266b42351c20daa48bc
PirateKing <golapoanthony25@gmail.com>
2026-07-03 09:33:34 +0800

    Hoist report modals out of sidebar on startup

--- src/js/app-bootstrap.js
+++ src/js/app-bootstrap.js
@@ -1,3 +1,17 @@
+function hoistModalToBody(modalId) {
+    const modal = document.getElementById(modalId);
+    if (!modal || modal.parentElement === document.body) {
+        return;
+    }
+
+    document.body.appendChild(modal);
+}
+
+[
+    "safeReportModal",
+    "senderDetailsModal"
+].forEach(hoistModalToBody);
+
 if (typeof window.initRoleAccess === "function") {
     window.initRoleAccess();
 }
